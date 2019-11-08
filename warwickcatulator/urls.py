9c38c3d2b1b7274939eb1558d34d36b8d1c133c6
Max Wilkinson <maxwilkinson99@gmail.com>
2019-11-08 15:19:59 +0000

    Added home page

--- warwickcatulator/urls.py
+++ warwickcatulator/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import TemplateView
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', include('social_django.urls', namespace='social')),
 ]
