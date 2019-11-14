6f5f8b47fab7309586d9962a2b9f3e01694c8a37
Max Wilkinson <maxwilkinson99@gmail.com>
2019-11-14 18:57:10 +0000

    Updated templates

--- results/forms.py
+++ results/forms.py
@@ -10,3 +10,8 @@ class ModuleForm(forms.ModelForm):
         model = Module
         fields = ['module_code', 'academic_year']
         
+    def __init__(self, *args, **kwargs):
+        super(ModuleForm, self).__init__(*args, **kwargs)
+        self.fields['module_code'].widget.attrs['class'] = 'form-control mb-2'
+        self.fields['module_code'].label = 'Module'
+        self.fields['academic_year'].widget.attrs['class'] = 'form-control'
