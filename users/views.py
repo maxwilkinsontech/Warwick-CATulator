5ffcf311bfae2948867a5d73400c01f9a7e70fdf
Max Wilkinson <maxwilkinson99@gmail.com>
2019-12-06 01:54:06 +0000

    changed callback url

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ def login_view(request):
     This is going to get a token from the Warwick oauth site and
     redirect the user to authorize this site.
     """
-    url = obtain_request_token()
+    url = obtain_request_token(callback='http://178.62.83.252:8000/callback/')
     return redirect(url)
 
 
