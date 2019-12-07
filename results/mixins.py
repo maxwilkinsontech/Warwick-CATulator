efe9b88ba4b0fd8208af42d555a48e6cf8904bc8
Max Wilkinson <maxwilkinson99@gmail.com>
2019-12-07 18:00:48 +0000

    Changed wording

--- results/mixins.py
+++ results/mixins.py
@@ -4,6 +4,7 @@ class ModuleResultPermissionMixin(object):
 
     def get_object(self, *args, **kwargs):
         obj = super(ModuleResultPermissionMixin, self).get_object(*args, **kwargs)
+        # Make sure using requesting ModuleResult is the user who owns it.
         if not obj.user == self.request.user:
             raise PermissionDenied()
         else:
